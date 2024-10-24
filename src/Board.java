ae47e6b43fc7f6e0eed7f3a4a957dedf0348e5ea
derossca <derossca@mail.uc.edu>
2024-10-24 13:47:25 -0400

    finished board

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Board {
+    private TicTacToeTile[][] board = new TicTacToeTile[3][3];
+
+    public Board(JPanel panel) {
+        panel.setLayout(new GridLayout(3, 3));
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                board[r][c] = new TicTacToeTile(r, c);
+                panel.add(board[r][c]);
+            }
+        }
+    }
+
+    public TicTacToeTile[][] getBoard() {
+        return board;
+    }
 }
